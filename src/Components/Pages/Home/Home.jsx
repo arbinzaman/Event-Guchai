3e44fb83c167e67ee84a08d4100c48b2c83a9c60
unknown <arbinzmn@gmail.com>
2024-04-29 09:19:28 +0600

    Added Feature Section

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Card from "./Card/Card";
 import HeroSection from "./HeroSection";
 import ReviewSection from "./ReviewSection/ReviewSection";
 import ServiceTestimonial from "./ServiceTestimonial/ServiceTestimonial";
+import FeatureSection from "./ServiceTestimonial/ServiceTestimonial";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection></HeroSection>
       <Card></Card>
       <ServiceTestimonial></ServiceTestimonial>
+      <FeatureSection></FeatureSection>
       <ReviewSection></ReviewSection>
     </div>
   );
