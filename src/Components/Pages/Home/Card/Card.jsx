705c4cb1fb3bae8a93d13c512d09df59e53d5552
unknown <arbinzmn@gmail.com>
2024-04-29 08:54:07 +0600

    Added Service Testimonial Section

--- src/Components/Pages/Home/Card/Card.jsx
+++ src/Components/Pages/Home/Card/Card.jsx
@@ -1,7 +1,7 @@
 const Card = () => {
   return (
-    <div>
-      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    <div className="container columns-3 mb-10">
+      <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
           <img
             src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
@@ -9,10 +9,52 @@ const Card = () => {
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title">
+            Shoes!
+            <div className="badge badge-secondary">Most Used</div>
+          </h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Shoes!
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            Shoes!
+          </h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
           </div>
         </div>
       </div>
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
-
+import Card from "./Card/Card";
 import HeroSection from "./HeroSection";
 import ReviewSection from "./ReviewSection/ReviewSection";
+import ServiceTestimonial from "./ServiceTestimonial/ServiceTestimonial";
 
 const Home = () => {
   return (
     <div>
       <HeroSection></HeroSection>
-
+      <Card></Card>
+      <ServiceTestimonial></ServiceTestimonial>
       <ReviewSection></ReviewSection>
     </div>
   );
