f5b7e8da6199f4926c9bd7a031dc00cedfb2cd9b
unknown <arbinzmn@gmail.com>
2024-04-29 09:05:44 +0600

    Changed color of theme

--- src/Components/Pages/Home/Card/Card.jsx
+++ src/Components/Pages/Home/Card/Card.jsx
@@ -3,7 +3,7 @@ const Card = () => {
     <section className="py-20  text-black-100">
     <div className="container px-4 mx-auto">
       <div className="max-w-2xl mx-auto mb-16 text-center">
-        <span className="font-bold tracking-wider uppercase text--400">Pricing</span>
+        <span className="font-bold tracking-wider uppercase text-accent">Pricing</span>
         <h2 className="text-4xl font-bold lg:text-5xl">Choose your best plan</h2>
       </div>
       <div className="flex flex-wrap items-stretch -mx-4">
@@ -16,29 +16,29 @@ const Card = () => {
             <p className="mt-3 leading-relaxed text-black-400">Etiam ac convallis enim, eget euismod dolor.</p>
             <ul className="flex-1 mb-6 text-black-400">
               <li className="flex mb-2 space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Aenean quis</span>
               </li>
               <li className="flex mb-2 space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Morbi semper</span>
               </li>
               <li className="flex mb-2 space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Tristique enim nec</span>
               </li>
             </ul>
-            <button type="button" className="inline-block px-5 py-3 font-semibold tracking-wider text-center rounded bg--400 text-black-900">Get Started</button>
+            <button type="button" className="inline-block px-5 py-3 font-semibold tracking-wider text-center rounded bg-accent text-black-900">Get Started</button>
           </div>
         </div>
         <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
-          <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 bg--400 text-black-900">
+          <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-accent text-black-900">
             <div className="space-y-2">
               <h4 className="text-2xl font-bold">Pro</h4>
               <span className="text-6xl font-bold">$24
@@ -72,7 +72,7 @@ const Card = () => {
                 <span>Aenean et lectus blandit</span>
               </li>
             </ul>
-            <a rel="noopener noreferrer" href="#" className="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded bg-black-800 text--400">Get Started</a>
+            <a rel="noopener noreferrer" href="#" className="inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded bg-black-800 text-accent">Get Started</a>
           </div>
         </div>
         <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
@@ -86,37 +86,37 @@ const Card = () => {
             <p className="leading-relaxed text-black-400">Phasellus ultrices bibendum nibh in vehicula.</p>
             <ul className="space-y-2 text-black-400">
               <li className="flex items-start space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Everything in Pro</span>
               </li>
               <li className="flex items-start space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Fusce sem ligula</span>
               </li>
               <li className="flex items-start space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Curabitur dictum</span>
               </li>
               <li className="flex items-start space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Duis odio eros</span>
               </li>
               <li className="flex items-start space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text--400">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6 text-accent">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                 </svg>
                 <span>Vivamus ut lectus ex</span>
               </li>
             </ul>
-            <a rel="noopener noreferrer" href="#" className="inline-block w-full px-5 py-3 font-semibold tracking-wider text-center rounded bg--400 text-black-900">Get Started</a>
+            <a rel="noopener noreferrer" href="#" className="inline-block w-full px-5 py-3 font-semibold tracking-wider text-center rounded bg-accent text-black-900">Get Started</a>
           </div>
         </div>
       </div>
