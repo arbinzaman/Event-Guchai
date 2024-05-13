5e9664c6ff00c917f8f9854ff260b2ad87ad3872
unknown <arbinzmn@gmail.com>
2024-05-13 20:44:08 +0600

    Fixed Dashboard bugs

--- src/Components/Layouts/DashboardLayouts/DashboardLayouts.jsx
+++ src/Components/Layouts/DashboardLayouts/DashboardLayouts.jsx
@@ -12,7 +12,7 @@ const DashBoardLayouts = () => {
       <NavBar></NavBar>
       <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
+        <div className="drawer-content flex flex-col ">
           <Outlet></Outlet>
           <label
             htmlFor="my-drawer-2"
