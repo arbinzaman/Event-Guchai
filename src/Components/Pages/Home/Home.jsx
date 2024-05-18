0b5349d9ac1980c007e24f78a441d1871288f0d9
arbinzaman <arbinzmn@gmail.com>
2024-05-18 17:39:22 +0600

    Added Delete Button

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import ReviewSection from "./ReviewSection/ReviewSection";
 import ServiceTestimonial from "./ServiceTestimonial/ServiceTestimonial";
 import FeatureSection from "./FeatureSection/FeatureSection";
 import TrustedBanner from "./TrustedBanner/TrustedBanner";
+import TimeLine from "./TimeLine/TimeLine";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <HeroSection></HeroSection>
       <Card></Card>
       <ServiceTestimonial></ServiceTestimonial>
+      <TimeLine></TimeLine>
       <FeatureSection></FeatureSection>
       <TrustedBanner></TrustedBanner>
       <ReviewSection></ReviewSection>
