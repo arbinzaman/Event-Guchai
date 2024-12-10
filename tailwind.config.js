023f242eea2c0c6194a20923417d049fb12a3248
arbinzaman <arbinzmn@gmail.com>
2024-12-11 00:04:57 +0600

    Fixed : Fixed Dark mood issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    themes: ["light", "cupcake", "pastel"],
+    extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: ["light"], // Only enable the light theme
+  },
 };
